e4d3930c40758ad3d4ab98668f134945bfb7bf58
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2019-04-23 22:37:19 +0700

    add columns

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -32,9 +32,31 @@
 
     <div class="columns is-multiline" v-show="config.withHeader">
       <div class="column is-2" v-for="(header, index) in config.headers" :key="index">
-        <b-field :label="`Header Column ${index + 1}`">
+        <b-field :label="`Header ${index + 1}`">
           <b-input v-model="config.headers[index]"></b-input>
         </b-field>
+      </div>
+    </div>
+
+    <div class="columns is-multiline" v-show="columnCount">
+      <div class="column is-2" v-for="(column, index) in config.columns" :key="index">
+        <b-field :label="`Column ${index + 1}`">
+          <b-select v-model="config.columns[index]" expanded>
+            <optgroup v-for="(option, indexOpt) in options" :key="indexOpt" :label="capitalize(option.group)">
+                <option v-for="(item, indexItem) in option.items" :key="indexItem" :value="item">
+                  {{ item.label }}
+                </option>
+            </optgroup>
+          </b-select>
+        </b-field>
+      </div>
+    </div>
+
+    <div class="columns is-multiline">
+      <div class="column">
+        <div class="buttons is-centered">
+          <b-button type="is-primary" rounded>DOWNLOAD</b-button>
+        </div>
       </div>
     </div>
   </div>
@@ -50,24 +72,103 @@
           delimiter: ';',
           withHeader: false,
           headers: [],
+          columns: [
+            { group: 'name', value: 'findName', label: 'Find Name' },
+            { group: 'name', value: 'findName', label: 'Find Name' },
+            { group: 'name', value: 'findName', label: 'Find Name' },
+            { group: 'name', value: 'findName', label: 'Find Name' },
+            { group: 'name', value: 'findName', label: 'Find Name' },
+            { group: 'name', value: 'findName', label: 'Find Name' },
+          ],
         },
+        options: [
+          {
+            group: 'address',
+            items: [
+              { group: 'address', value: 'zipCode', label: 'Zip Code' },
+              { group: 'address', value: 'city', label: 'City' },
+              { group: 'address', value: 'streetAddress', label: 'Street Address' },
+              { group: 'address', value: 'country', label: 'Country' },
+              { group: 'address', value: 'state', label: 'State' },
+              { group: 'address', value: 'latitude', label: 'Latitude' },
+              { group: 'address', value: 'longitude', label: 'Longitude' },
+            ],
+          },
+          {
+            group: 'company',
+            items: [
+              { group: 'company', value: 'companyName', label: 'Company Name' },
+            ],
+          },
+          {
+            group: 'date',
+            items: [
+              { group: 'date', value: 'past', label: 'Past' },
+              { group: 'date', value: 'future', label: 'Future' },
+              { group: 'date', value: 'between', label: 'Between' },
+              { group: 'date', value: 'recent', label: 'Recent' },
+            ],
+          },
+          {
+            group: 'internet',
+            items: [
+              { group: 'internet', value: 'email', label: 'Email' },
+              { group: 'internet', value: 'userName', label: 'Username' },
+              { group: 'internet', value: 'password', label: 'Password' },
+              { group: 'internet', value: 'url', label: 'URL' },
+            ],
+          },
+          {
+            group: 'lorem',
+            items: [
+              { group: 'lorem', value: 'words', label: 'Words' },
+              { group: 'lorem', value: 'sentence', label: 'Sentence' },
+              { group: 'lorem', value: 'sentences', label: 'Sentences' },
+              { group: 'lorem', value: 'paragraph', label: 'Paragraph' },
+              { group: 'lorem', value: 'paragraphs', label: 'Paragraphs' },
+            ],
+          },
+          {
+            group: 'name',
+            items: [
+              { group: 'name', value: 'firstName', label: 'First Name' },
+              { group: 'name', value: 'lastName', label: 'Last Name' },
+              { group: 'name', value: 'findName', label: 'Find Name' },
+            ],
+          },
+          {
+            group: 'phone',
+            items: [
+              { group: 'phone', value: 'phoneNumber', label: 'Phone Number' },
+              { group: 'phone', value: 'phoneNumberFormat', label: 'Phone Number Format' },
+            ],
+          },
+          {
+            group: 'random',
+            items: [
+              { group: 'random', value: 'number', label: 'Number' },
+              { group: 'random', value: 'uuid', label: 'UUID' },
+            ],
+          },
+        ],
       }
     },
     computed: {
       columnCount() {
-        return this.config.columnCount
-      }
+        return parseInt(this.config.columnCount)
+      },
     },
     watch: {
       columnCount(newValue, oldValue) {
         this.populateHeaders()
+
+        this.populateColumns()
       }
     },
     methods: {
       populateHeaders() {
-        let {columnCount, withHeader, headers} = this.config
-
-        columnCount = parseInt(columnCount)
+        let {withHeader, headers} = this.config
+        let columnCount = this.columnCount
 
         if (withHeader) {
           if (headers.length <= columnCount) {
@@ -81,6 +182,24 @@
             headers.splice(columnCount)
           }
         }
+      },
+      populateColumns() {
+        let {columns} = this.config
+        let columnCount = this.columnCount
+
+        if (columns.length <= columnCount) {
+          for (let index = 0; index < columnCount; index++) {
+            if (!columns[index]) {
+              columns[index] = {}
+            }
+          }
+        }
+        else {
+          columns.splice(columnCount)
+        }
+      },
+      capitalize(value) {
+        return value.charAt(0).toUpperCase() + value.slice(1)
       },
     },
   }
